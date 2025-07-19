6e595ad6c9a9f5768c2555831a1853f50e583788
endoeduardo <eduardo.t.endo@gmail.com>
2025-07-19 16:10:50 -0300

    FEAT: add styling

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type PluginOption } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const fullReloadAlways: PluginOption = {
+  name: 'full-reload-always',
+  handleHotUpdate({ server }) {
+    server.ws.send({ type: 'full-reload' })
+    return []
+  }
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), fullReloadAlways],
+  server: {
+    watch: { usePolling: true },
+    port: 3000,
+  }
 })
